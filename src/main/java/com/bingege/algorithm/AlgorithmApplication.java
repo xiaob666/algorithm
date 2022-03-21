33f08617fb5478be7fcbafd89ed860db9abb86f6
xiaobin <xiaobin@mockuai.com>
2022-03-21 23:22:35 +0800

    2022-03-21

---
+++ src/main/java/com/bingege/algorithm/AlgorithmApplication.java
@@ -0,0 +1,12 @@
+package com.bingege.algorithm;
+
+/**
+ * 算法基础
+ * @author xiaob
+ */
+public class AlgorithmApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/bingege/algorithm/array/两数之和_1.java
@@ -0,0 +1,68 @@
+package com.bingege.algorithm.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author xiaob
+ * @date 2022-03-21
+ */
+public class 两数之和_1 {
+
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[0,1]
+     * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+     * <p>
+     * 提示：
+     * <p>
+     * 2 <= nums.length <= 104
+     * -109 <= nums[i] <= 109
+     * -109 <= target <= 109
+     * 只会存在一个有效答案
+     * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+     */
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(twoSum(nums, target));
+    }
+
+    /**
+     * 时间复杂度 O(n^2)
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        int before, after;
+        for (int i = 0; i < nums.length - 1; i++) {
+            before = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                after = nums[j];
+                if (after + before == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 时间复杂度 O(n)
+     */
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+            for (int i = 0; i < nums.length; ++i) {
+                //每次判断 差值是否已经在 table中
+                if (hashtable.containsKey(target - nums[i])) {
+                    return new int[]{hashtable.get(target - nums[i]), i};
+                }
+                hashtable.put(nums[i], i);
+            }
+            return new int[0];
+        }
+    }
+}
